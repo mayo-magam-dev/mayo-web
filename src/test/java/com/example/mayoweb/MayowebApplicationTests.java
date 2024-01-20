e225f84bf22310f3bd17e1c512d38caadefc7a41
wnstj1548 <wnstj0614@naver.com>
2024-01-20 23:30:01 +0900

    commit

---
+++ src/test/java/com/example/mayoweb/MayowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mayoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
