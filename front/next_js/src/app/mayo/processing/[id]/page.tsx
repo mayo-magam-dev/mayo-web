a3eed234b7e7b6b2b0b9ad47ed1b14ee4699ff65
박승찬 <seungchan141414@gmail.com>
2024-03-01 04:00:53 +0900

    [Feat : front] processing 페이지 id 정보 가져오기

--- front/next_js/src/app/mayo/processing/[id]/page.tsx
+++ front/next_js/src/app/mayo/processing/[id]/page.tsx
@@ -1,19 +1,32 @@
 "use client";
 // http://localhost:3000/mayo/processing/Asb0QBuQ9gmEyyWlSVhY
 import MayoHeader from "@/atoms/molecule/mayoHeader";
+import MayoProcessingMain from "@/atoms/molecule/mayoProcessingMain";
 import NavigationLeft from "@/atoms/molecule/navigationLeft";
-import NewOrder from "@/atoms/molecule/newOrder";
-
+import NewOrder from "@/atoms/molecule/newOrders";
+import { usePathname } from "next/navigation";
 export default function MayoProcessing() {
+  const pathName: string = usePathname();
+  let id: string | undefined;
+
+  if (typeof pathName === "string") {
+    id = pathName.split("/").pop();
+  } else {
+    // pathName이 undefined인 경우에 대한 처리
+    id = undefined;
+  }
+
   return (
-    <main className="rounded-[3rem] w-[95%] h-[105rem] bg-blue m-[5rem]">
+    <main className="rounded-[2rem] w-[95%] h-[105rem] m-[5rem]">
       <header>
         <MayoHeader />
       </header>
       <div className="flex ">
-        <NavigationLeft />
+        <NavigationLeft id={id} />
         <div>
-          <div className="text-white text-6xl font-bold"></div>
+          <div>
+            <MayoProcessingMain />
+          </div>
           <div className="text-white text-6xl font-bold">2</div>
           <div></div>
         </div>
