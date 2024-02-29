cff633029d5455623f75204d868e2e3de3bb3a24
박승찬 <seungchan141414@gmail.com>
2024-03-01 06:36:57 +0900

    [Feat : front] newOrder modal등록 및 작업

--- front/next_js/src/atoms/molecule/newOrders.tsx
+++ front/next_js/src/atoms/molecule/newOrders.tsx
@@ -2,20 +2,30 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import NewOrder from "./newOrder";
+import NewOrderDetailModal from "../atom/newOrderDetailModal";
 
 export default function NewOrders() {
-  const router = useRouter();
+  const [selectedNewOrderId, setSelectedNewOrderId] = useState<string | null>(
+    null
+  );
+
+  const handleNewOrderOpenModal = (newOrderId: string) => {
+    setSelectedNewOrderId(newOrderId);
+  };
 
   return (
-    <div className="text-[#F0BD22]">
+    <div className="text-[#F0BD22] ">
       <div className="flex items-center h-[8rem] text-7xl font-bold pl-[2rem] bg-[#25272B]">
         신규 1건
       </div>
-      <div className="h-[25rem] overflow-y-auto">
-        <NewOrder />
-        <NewOrder />
-        <NewOrder />
+      <div className="h-[25rem] overflow-y-auto font-bold">
+        <NewOrder newOrderId={"1"} onClick={handleNewOrderOpenModal} />
+        <NewOrder newOrderId={"2"} onClick={handleNewOrderOpenModal} />
+        <NewOrder newOrderId={"3"} onClick={handleNewOrderOpenModal} />
       </div>
+      {selectedNewOrderId !== null && (
+        <NewOrderDetailModal newOrderId={selectedNewOrderId} />
+      )}
     </div>
   );
 }
