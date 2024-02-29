c1600ee77e6cae867cdd6f0f8436ebe7230a2a70
박승찬 <seungchan141414@gmail.com>
2024-03-01 06:35:51 +0900

    [Feat : front] newOrder modal등록 및 작업

--- front/next_js/src/atoms/molecule/mayoProcessingMain.tsx
+++ front/next_js/src/atoms/molecule/mayoProcessingMain.tsx
@@ -3,12 +3,15 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import NewOrders from "./newOrders";
 import ProcessingOrders from "./processingOrders";
+import NewOrderDetailModal from "../atom/newOrderDetailModal";
 
 export default function MayoProcessingMain() {
-  const router = useRouter();
+  const [selectedNewOrderId, setSelectedNewOrderId] = useState<string | null>(
+    null
+  );
 
   return (
-    <div className="h-[90rem] text-6xl font-bold bg-[#0E0E0E] w-[80rem]">
+    <div className="h-[90rem] text-6xl  bg-[#0E0E0E] w-[80rem]">
       <NewOrders />
       <ProcessingOrders />
     </div>
--- front/next_js/src/atoms/molecule/newOrder.tsx
+++ front/next_js/src/atoms/molecule/newOrder.tsx
@@ -2,11 +2,19 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function NewOrder() {
+interface NewOrderProps {
+  newOrderId: string;
+  onClick: (newOrderId: string) => void;
+}
+
+export default function NewOrder(props: NewOrderProps) {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col text-[#CFCFCF] justify-center text-4xl h-[14rem] pl-[2rem] bg-[#333439]">
+    <div
+      className="flex flex-col text-[#CFCFCF] justify-center text-4xl h-[14rem] pl-[2rem] bg-[#333439]"
+      onClick={() => props.onClick(props.newOrderId)}
+    >
       <div className="flex items-center mb-[1rem]">
         <div>주문 메뉴:</div>
         <div className="text-6xl font-bold">함박 촉촉 스테이크</div>
