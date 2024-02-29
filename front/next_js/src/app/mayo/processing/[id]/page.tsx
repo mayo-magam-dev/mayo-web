2873da96105623bba878d44808357803b80fd070
박승찬 <seungchan141414@gmail.com>
2024-03-01 01:36:16 +0900

    [Feat : front] mayo Header, NavigationLeft 기능 추가

--- front/next_js/src/app/mayo/processing/[id]/page.tsx
+++ front/next_js/src/app/mayo/processing/[id]/page.tsx
@@ -1,13 +1,24 @@
-'use client';
+"use client";
 // http://localhost:3000/mayo/processing/Asb0QBuQ9gmEyyWlSVhY
 import MayoHeader from "@/atoms/molecule/mayoHeader";
+import NavigationLeft from "@/atoms/molecule/navigationLeft";
+import NewOrder from "@/atoms/molecule/newOrder";
 
 export default function MayoProcessing() {
   return (
-    <main className="rounded w-[100%] h-[130rem] bg-red">
+    <main className="rounded-[3rem] w-[95%] h-[105rem] bg-blue m-[5rem]">
       <header>
-        <MayoHeader  />
+        <MayoHeader />
       </header>
+      <div className="flex ">
+        <NavigationLeft />
+        <div>
+          <div className="text-white text-6xl font-bold"></div>
+          <div className="text-white text-6xl font-bold">2</div>
+          <div></div>
+        </div>
+        <div>2</div>
+      </div>
     </main>
   );
 }
