e044dce38b7780a0801dbeae88c21e451ad49293
박승찬 <seungchan141414@gmail.com>
2024-03-01 07:26:30 +0900

    [Feat : front] newOrder, processing 모달 등록

--- front/next_js/src/atoms/molecule/processingOrders.tsx
+++ front/next_js/src/atoms/molecule/processingOrders.tsx
@@ -1,25 +1,21 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import NewOrder from "./newOrder";
 import ProcessingOrder from "./processingOrder";
+import ProcessingOrderDetailModal from "../atom/ProcessingOrderDetailModal";
 
 export default function ProcessingOrders() {
-  const router = useRouter();
-
   return (
     <div className="text-[#F0BD22]">
       <div className="flex items-center h-[8rem] text-7xl font-bold pl-[2rem] bg-[#25272B]">
         진행 1건
       </div>
       <div className="h-[49rem] overflow-y-auto">
-        <ProcessingOrder />
-        <ProcessingOrder />
-        <ProcessingOrder />
-        <ProcessingOrder />
-        <ProcessingOrder />
-        <ProcessingOrder />
-        <ProcessingOrder />
+        <ProcessingOrder processingOrderId={"1"} />
+        <ProcessingOrder processingOrderId={"1"} />
+        <ProcessingOrder processingOrderId={"1"} />
+        <ProcessingOrder processingOrderId={"1"} />
+        <ProcessingOrder processingOrderId={"1"} />
       </div>
     </div>
   );
