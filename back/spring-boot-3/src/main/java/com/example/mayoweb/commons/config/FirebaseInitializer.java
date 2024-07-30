4ff74417a187e4acf646e8f13da658ae1effab24
wnstj1548 <wnstj0614@naver.com>
2024-07-30 14:06:23 +0900

    feat : "API로 변경 및 리팩토링"

---
+++ back/spring-boot-3/src/main/java/com/example/mayoweb/commons/config/FirebaseInitializer.java
@@ -0,0 +1,40 @@
+package com.example.mayoweb.commons.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+@Configuration
+@Slf4j
+public class FirebaseInitializer {
+
+    private static final String DATABASE_URL = "https://mayo-app.firebaseio.com";
+
+    @PostConstruct
+    public void init() {
+        try {
+            InputStream inputStream = new ClassPathResource(
+                    "/mayo-app-280d4.json").getInputStream();
+            File file = File.createTempFile("key", ".json");
+            FileCopyUtils.copy(inputStream.readAllBytes(), file);
+            FileInputStream firebaseAccount = new FileInputStream(file);
+            FirebaseOptions.Builder optionBuilder = FirebaseOptions.builder();
+            FirebaseOptions options = optionBuilder
+                    .setCredentials(GoogleCredentials.fromStream(firebaseAccount))
+                    .setDatabaseUrl(DATABASE_URL)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+}
---
+++ back/spring-boot-3/src/main/java/com/example/mayoweb/items/controller/ItemRestController.java
@@ -0,0 +1,61 @@
+package com.example.mayoweb.items.controller;
+
+import com.example.mayoweb.items.domain.request.UpdateItemRequest;
+import com.example.mayoweb.items.domain.response.ReadFirstItemResponse;
+import com.example.mayoweb.items.service.ItemsService;
+import com.example.mayoweb.items.domain.request.CreateItemRequest;
+import com.example.mayoweb.items.domain.response.ReadItemResponse;
+import com.example.mayoweb.reservation.domain.dto.response.ReadReservationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ItemRestController {
+
+    private final ItemsService itemsService;
+
+    @GetMapping("/item")
+    public ResponseEntity<ReadItemResponse> getItemById(@RequestParam String itemId) {
+        return ResponseEntity.ok(itemsService.getItemById(itemId));
+    }
+
+    @GetMapping("/item-store")
+    public ResponseEntity<List<ReadItemResponse>> getItemsByStoreId(@RequestParam String storeId) {
+        return ResponseEntity.ok(itemsService.getItemsByStoreId(storeId));
+    }
+
+    @PostMapping("/item")
+    public ResponseEntity<Void> createItem(@RequestBody CreateItemRequest request, String storeId) {
+
+        itemsService.save(request, storeId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/item")
+    public ResponseEntity<Void> updateItem(@RequestBody UpdateItemRequest request) {
+
+        itemsService.updateItem(request);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/item")
+    public ResponseEntity<Void> deleteItem(@RequestParam String itemId) {
+
+        itemsService.deleteItem(itemId);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/first-item")
+    public ResponseEntity<List<ReadFirstItemResponse>> getFirstItemByReservations(@RequestBody List<ReadReservationResponse> reservations) {
+
+        return ResponseEntity.ok(itemsService.getFirstItemNamesFromReservations(reservations));
+    }
+
+}
