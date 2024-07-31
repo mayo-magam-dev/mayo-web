0986a303f2e776786bfa83bae69411e53be360bc
wnstj1548 <wnstj0614@naver.com>
2024-07-31 12:54:50 +0900

    feat : "fcm 기능 구현"

---
+++ back/spring-boot-3/src/main/java/com/example/mayoweb/fcm/repository/WebPushNotificationsAdapter.java
@@ -0,0 +1,34 @@
+package com.example.mayoweb.fcm.repository;
+
+import com.example.mayoweb.fcm.dto.WebPushNotificationsDto;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.WriteResult;
+
+@Repository
+@Slf4j
+public class WebPushNotificationsAdapter {
+
+    private static final String COLLECTION_NAME = "web_push_notifications";
+
+    public boolean addWebPushNotifications(WebPushNotificationsDto entity) {
+        Firestore firestore = FirestoreClient.getFirestore();
+        DocumentReference addDocRef = firestore.collection(COLLECTION_NAME).document();
+
+        try {
+            ApiFuture<WriteResult> writeResult = addDocRef.set(entity);
+
+            writeResult.get();
+
+            return true;
+        } catch (Exception e) {
+            log.error("addWebPushNotifications error" + e.getMessage());
+            return false;
+        }
+    }
+}
