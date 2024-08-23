18fe4d5328820679a5f138a1a9b1aee0bffbd595
wnstj1548 <wnstj0614@naver.com>
2024-08-23 09:41:22 +0900

    feat : "파일 업로드 수정"

--- src/main/java/com/example/mayoweb/storage/service/StorageService.java
+++ src/main/java/com/example/mayoweb/storage/service/StorageService.java
@@ -1,6 +1,7 @@
 package com.example.mayoweb.storage.service;
 
-import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Acl;
+import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Bucket;
 import com.google.firebase.cloud.StorageClient;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @Service
 public class StorageService {
@@ -17,12 +19,22 @@ public class StorageService {
 
     // 파일 업로드
     public String uploadFirebaseBucket(MultipartFile multipartFile, String fileName) throws IOException {
+
         Bucket bucket = StorageClient.getInstance().bucket(firebaseStorageUrl);
 
-        Blob blob = bucket.create(fileName,
-                multipartFile.getInputStream(), multipartFile.getContentType());
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucket.getName(), fileName)
+                .setContentType(multipartFile.getContentType())
+                .build();
 
-        return blob.getMediaLink();
+        bucket.create(fileName, multipartFile.getInputStream(), multipartFile.getContentType());
+
+        bucket.get(fileName).createAcl(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
+
+        String imageUrl = String.format("https://storage.googleapis.com/%s/%s",
+                bucket.getName(),
+                URLEncoder.encode(fileName, "UTF-8"));
+
+        return imageUrl;
     }
 
     // 파일 삭제
