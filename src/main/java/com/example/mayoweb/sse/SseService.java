5bb233f7f9c4bfed2fe4844b082301860c6e0ffb
wnstj1548 <wnstj0614@naver.com>
2024-09-07 15:30:25 +0900

    feat : "sse 변경"

--- src/main/java/com/example/mayoweb/sse/SseService.java
+++ src/main/java/com/example/mayoweb/sse/SseService.java
@@ -10,26 +10,23 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Service
 public class SseService {
 
-    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
-    private final SseEmitter emitter = new SseEmitter();
+    private SseEmitter emitter;
 
-    public void addEmitter(SseEmitter emitter) {
-        emitters.add(emitter);
-        emitter.onCompletion(() -> emitters.remove(emitter));
-        emitter.onTimeout(() -> emitters.remove(emitter));
+    // 새로운 Emitter 추가
+    public SseEmitter addEmitter() {
+        emitter = new SseEmitter(0L);
+        emitter.onCompletion(() -> emitter = null);  // 완료 시 null 처리
+        emitter.onTimeout(() -> emitter = null);     // 타임아웃 시 null 처리
+        return emitter;
     }
 
-    public void sendMessageToEmitter(String message, String name) throws IOException {
-        emitter.send(SseEmitter.event().name(name).data(message));
-    }
-
-    public void sendMessageToEmitters(String message, String name) {
-
-        for (SseEmitter emitter : emitters) {
+    // 단일 Emitter에게 메시지 전송
+    public void sendMessage(String message, String name) {
+        if (emitter != null) {
             try {
-                    emitter.send(SseEmitter.event().name(name).data(message));
-                } catch (IOException e) {
-                throw new RuntimeException(e);
+                emitter.send(SseEmitter.event().name(name).data(message));
+            } catch (IOException e) {
+                emitter = null;  // 전송 실패 시 Emitter 초기화
             }
         }
     }
