e85549b780b206de5695b2a22cbb684dd0838268
wnstj1548 <wnstj0614@naver.com>
2024-09-07 16:23:56 +0900

    feat : "sse 변경 client당 하나로 변경"

--- src/main/java/com/example/mayoweb/sse/SseService.java
+++ src/main/java/com/example/mayoweb/sse/SseService.java
@@ -4,29 +4,35 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Service
 public class SseService {
 
-    private SseEmitter emitter;
+    private final ConcurrentMap<String, SseEmitter> emitters = new ConcurrentHashMap<>();
 
-    // 새로운 Emitter 추가
-    public SseEmitter addEmitter() {
-        emitter = new SseEmitter(0L);
-        emitter.onCompletion(() -> emitter = null);  // 완료 시 null 처리
-        emitter.onTimeout(() -> emitter = null);     // 타임아웃 시 null 처리
+    public SseEmitter addEmitter(String clientId) {
+        SseEmitter emitter = new SseEmitter(0L);
+        emitters.put(clientId, emitter);
+        emitter.onCompletion(() -> emitters.remove(clientId));  // 완료 시 제거
+        emitter.onTimeout(() -> emitters.remove(clientId));     // 타임아웃 시 제거
         return emitter;
     }
 
-    // 단일 Emitter에게 메시지 전송
-    public void sendMessage(String message, String name) {
+    public SseEmitter getEmitter(String clientId) {
+        return emitters.get(clientId);
+    }
+
+    public void sendMessageToClient(String clientId, String message, String name) {
+
+        SseEmitter emitter = getEmitter(clientId);
+
         if (emitter != null) {
             try {
                 emitter.send(SseEmitter.event().name(name).data(message));
             } catch (IOException e) {
-                emitter = null;  // 전송 실패 시 Emitter 초기화
+                emitters.remove(clientId);
             }
         }
     }
