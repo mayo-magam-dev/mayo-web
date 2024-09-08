0a049f4a6db4d7aca81fa750e88f95ddcf0c5211
wnstj1548 <wnstj0614@naver.com>
2024-09-08 14:23:52 +0900

    feat : "item Validation 추가"

--- src/main/java/com/example/mayoweb/store/domain/dto/request/UpdateStoreRequest.java
+++ src/main/java/com/example/mayoweb/store/domain/dto/request/UpdateStoreRequest.java
@@ -25,7 +25,7 @@ public record UpdateStoreRequest
             @Pattern(regexp = "^([01]?\\d|2[0-3]):([0-5]\\d)$", message = "올바르지 않은 형식입니다.")
             String saleEnd,
 
-            @Max(value = 300, message = "최대 300글자입니다.")
+            @Size(max = 300, message = "최대 300글자입니다.")
             String additionalComment,
 
             @Pattern(regexp = "^(02|\\d{3,4})-\\d{3,4}-\\d{4}$", message = "올바르지 않은 형식입니다.")
