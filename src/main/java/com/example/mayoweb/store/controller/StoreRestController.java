4702a8ec9e841cd76602dff265e9f466b4223d0d
wnstj1548 <wnstj0614@naver.com>
2024-09-10 00:04:07 +0900

    feat : "cors 설정"

--- src/main/java/com/example/mayoweb/store/controller/StoreRestController.java
+++ src/main/java/com/example/mayoweb/store/controller/StoreRestController.java
@@ -28,7 +28,7 @@ import java.util.concurrent.ExecutionException;
 @Tag(name = "가게 API", description = "가게 관리 API")
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://www.mayomagam.store/")
 public class StoreRestController {
 
     private final StoresService storesService;
