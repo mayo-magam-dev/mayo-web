4f47fe2660c304978eceaa887aef9c396ca267ee
wnstj1548 <wnstj0614@naver.com>
2024-09-24 15:04:36 +0900

    feat : "cors 설정 변경"

--- src/main/java/com/example/mayoweb/commons/config/WebMvcConfig.java
+++ src/main/java/com/example/mayoweb/commons/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.mayoweb.commons.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,6 +18,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/static/img/",
                         "classpath:/static/css/",
                         "classpath:/static/js/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트에 적용
+                .allowedOrigins("https://www.mayomagam.store", "http://localhost:3000") // 허용할 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowCredentials(true); // 쿠키 허용 여부
     }
 
 }
