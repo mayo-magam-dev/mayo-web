873d7e49337dc0f23c5f1f27bf513f3d384f0869
wnstj1548 <wnstj0614@naver.com>
2024-10-12 12:52:49 +0900

    feat : "sse 버그수정"

--- src/main/java/com/example/mayoweb/MayowebApplication.java
+++ src/main/java/com/example/mayoweb/MayowebApplication.java
@@ -3,7 +3,6 @@ package com.example.mayoweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
