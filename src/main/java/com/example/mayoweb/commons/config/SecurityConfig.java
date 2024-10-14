f5d216f1ba936b21a9a65c66ade7f7509aed5d57
wnstj1548 <wnstj0614@naver.com>
2024-10-15 08:56:03 +0900

    refactor : "security 매칭 url 변경"

--- src/main/java/com/example/mayoweb/commons/config/SecurityConfig.java
+++ src/main/java/com/example/mayoweb/commons/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
-                        .requestMatchers("/reservation-new-async", "/sse/reservations-new", "/reservation-proceed-async","/reservations-new","/user", "/stores").permitAll()
+                        .requestMatchers("/reservation-new-async", "/sse/reservations-new", "/reservation-proceed-async","/reservations-new","/user", "/stores", "/item-store").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(new FirebaseAuthFilter(firestore), UsernamePasswordAuthenticationFilter.class);
         http
