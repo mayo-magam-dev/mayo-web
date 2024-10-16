dfb1bfe1db52ffa484753d4ae252cd34e0765c1a
wnstj1548 <wnstj0614@naver.com>
2024-10-16 14:39:44 +0900

    feat : "access ip 추가"

--- src/main/java/com/example/mayoweb/commons/manager/IpAccessAuthorizationManager.java
+++ src/main/java/com/example/mayoweb/commons/manager/IpAccessAuthorizationManager.java
@@ -1,5 +1,7 @@
 package com.example.mayoweb.commons.manager;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,22 +10,23 @@ import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Supplier;
 
 @Slf4j
 @Component
-public class IpAccessAuthorizationManager implements AuthorizationManager {
+@RequiredArgsConstructor
+public class IpAccessAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
 
     @Override
-    public AuthorizationDecision check(Supplier authentication, Object object) {
+    public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
 
         HttpServletRequest request = ((RequestAuthorizationContext) object).getRequest();
         String clientIp = getClientIp(request);
 
-        Set<String> allowedIps = new HashSet<>();
-        allowedIps.add("127.0.0.1");
+        Set<String> allowedIps = new HashSet<>(Arrays.asList("127.0.0.1", "0:0:0:0:0:0:0:1", "218.237.244.130"));
 
         return new AuthorizationDecision(allowedIps.contains(clientIp));
     }
