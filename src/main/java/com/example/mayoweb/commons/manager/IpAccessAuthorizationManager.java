b8d5fc7d715ff51bf4240f7707f61260619b9630
wnstj1548 <wnstj0614@naver.com>
2024-10-25 09:59:36 +0900

    feat : "spring security 설정"

--- src/main/java/com/example/mayoweb/commons/manager/IpAccessAuthorizationManager.java
+++ src/main/java/com/example/mayoweb/commons/manager/IpAccessAuthorizationManager.java
@@ -10,6 +10,8 @@ import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -26,7 +28,9 @@ public class IpAccessAuthorizationManager implements AuthorizationManager<Reques
         HttpServletRequest request = ((RequestAuthorizationContext) object).getRequest();
         String clientIp = getClientIp(request);
 
-        Set<String> allowedIps = new HashSet<>(Arrays.asList("127.0.0.1", "0:0:0:0:0:0:0:1", "218.237.244.130"));
+        Set<String> allowedDomains = new HashSet<>(Arrays.asList("mayomagam.store", "www.mayomagam.store", "localhost:3000"));
+        Set<String> allowedIps = resolveDomainToIp(allowedDomains);
+        allowedIps.add("127.0.0.1");
 
         return new AuthorizationDecision(allowedIps.contains(clientIp));
     }
@@ -41,5 +45,21 @@ public class IpAccessAuthorizationManager implements AuthorizationManager<Reques
 
         log.info("remote address : {}", request.getRemoteAddr());
         return request.getRemoteAddr();
+    }
+
+    private Set<String> resolveDomainToIp(Set<String> allowedDomains) {
+        Set<String> resolvedIps = new HashSet<>();
+
+        for (String domain : allowedDomains) {
+            try {
+                InetAddress[] addresses = InetAddress.getAllByName(domain);
+                for (InetAddress address : addresses) {
+                    resolvedIps.add(address.getHostAddress());
+                }
+            } catch (UnknownHostException e) {
+                log.error("Domain not found: {}", domain);
+            }
+        }
+        return resolvedIps;
     }
 }
