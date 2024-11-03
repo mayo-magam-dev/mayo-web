6190901a5729f22f53b28a5275523dce04a5b30f
wnstj1548 <wnstj0614@naver.com>
2024-11-03 23:52:21 +0900

    feat : "fcm 리스너 및 발송 추가"

--- src/main/java/com/example/mayoweb/fcm/entity/FCMToken.java
+++ src/main/java/com/example/mayoweb/fcm/entity/FCMToken.java
@@ -1,6 +1,7 @@
 package com.example.mayoweb.fcm.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.cloud.firestore.annotation.PropertyName;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,17 +12,23 @@ import lombok.ToString;
 @ToString
 public class FCMToken {
 
+    @PropertyName("fcm_token")
     @JsonProperty("fcm_token")
     private String token;
 
+    @PropertyName("created_at")
     @JsonProperty("created_at")
     private String createdAt;
 
+    @PropertyName("device_type")
     @JsonProperty("device_type")
     private String deviceType;
 
     @Builder
-    public FCMToken(String token, String createdAt, String deviceType) {
+    public FCMToken(
+            String token,
+            String createdAt,
+            String deviceType) {
         this.token = token;
         this.createdAt = createdAt;
         this.deviceType = deviceType;
