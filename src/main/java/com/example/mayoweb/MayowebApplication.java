5da43347824c6348a61c9d89b7133b376d002e0b
wnstj1548 <wnstj0614@naver.com>
2024-12-30 09:22:36 +0900

    feat : "시간 설정"

--- src/main/java/com/example/mayoweb/MayowebApplication.java
+++ src/main/java/com/example/mayoweb/MayowebApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class MayowebApplication {
@@ -12,4 +15,8 @@ public class MayowebApplication {
 		SpringApplication.run(MayowebApplication.class, args);
 	}
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
